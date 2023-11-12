29e27945dc7e9a5f00cb6fd33dc92e5aaf0b763f
Cozmeh <hemsainath15@gmail.com>
2023-11-12 15:31:42 +0530

    0.0.3

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
 DESCRIPTION = 'Search and summarize the web with ease!'
 
 # Setting up
 setup(
-    name="summersearch",
+    name="summer-search",
     version=VERSION,
     author="Cozmic (Hem Sainath)",
     author_email="hemsainath15@gmail.com",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['bs4', 'requests', 'transformers','sentencepiece','tensorflow','torch'],
     keywords=['python', 'search', 'summerisation', 'summary', 'ml', 'information'],
